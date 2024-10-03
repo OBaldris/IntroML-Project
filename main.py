83fa129345e09e2c30f1e8cead8640681205a798
OBaldris <oriolbaldris@gmail.com>
2024-10-03 11:56:27 +0200

    added box plot

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import pandas as pd
   
 # fetch dataset 
 cirrhosis_patient_survival_prediction = fetch_ucirepo(id=878) 
-  
+
+
+
 # data (as pandas dataframes) 
 X = cirrhosis_patient_survival_prediction.data.features 
 y = cirrhosis_patient_survival_prediction.data.targets 
   
 # metadata 
-print(cirrhosis_patient_survival_prediction.metadata) 
+#print(cirrhosis_patient_survival_prediction.metadata) 
   
 # variable information 
-print(cirrhosis_patient_survival_prediction.variables) 
+#print(cirrhosis_patient_survival_prediction.variables) 
 
 data = pd.DataFrame(X)
 
